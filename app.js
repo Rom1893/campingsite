63cc3e6d7e4008ed05678906ad9547f044c4545a
Rodrigo Morán <rodrigobq18@gmail.com>
2022-07-04 13:44:50 -0400

    Added EJS-mate
    Added EJS-mate to all ejs pages.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const path = require("path")
-const campground = require("./models/campground")
-const methodOverride = require("method-override")
+const path = require("path");
+const ejsmate = require("ejs-mate");
+const campground = require("./models/campground");
+const methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
@@ -20,6 +21,7 @@ app.set("view engine", "ejs" );
 app.set("views", path.join(__dirname, "views") );
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsmate);
 
 app.listen(3000, ()=> {
     console.log("Listening on port 3000")
