2ec1d94332c46716925698fad7cab52165d40464
Rodrigo Morán <rodrigobq18@gmail.com>
2022-08-08 00:14:14 -0400

    added start script

--- app.js
+++ app.js
@@ -144,7 +144,9 @@ app.use("/campgrounds/:id/reviews", reviewsRoutes);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(mongoSanitize());
 
-app.listen(3000, () => {
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log("Listening on port 3000")
 });
 /////////////////////////////////////////////////////////
